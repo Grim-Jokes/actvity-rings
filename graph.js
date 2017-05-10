70bf48eb899836d7dd72c9730b0f19ed2b7ffcc6
Daniel Szekely <danszekely@gmail.com>
2017-05-10 09:38:50 -0400

    Initial commit to render rigns

---
+++ graph.js
@@ -0,0 +1,93 @@
+function Graph(params, gl) {
+	this.x = params.x || 75;
+	this.y = params.y || 75;
+	this.radius = params.radius || 50;
+	this.gl = gl;
+	this.speed = params.speed || 1;
+	this.startAngle = params.startAngle  || 0;
+	
+
+	this.endAngles = [];
+
+	params.endAngles.forEach((item) => {
+		this.endAngles.push({
+			current: this.startAngle, 
+			target: (item.angle || item) + this.startAngle, 
+			color: item.color || "#000000" , 
+			faded_color: item.faded_color || "#444444",
+			complete: false});
+	});
+	
+	self.complete = false;
+}
+
+Graph.prototype = {
+	
+	_getDirection: function(index) {
+		return this.startAngle > this.endAngles[index].target ? -1 : 1;
+	},
+	
+	draw: function() {
+		var self = this;
+		
+		var animate = function() {
+			
+			self.gl.clearRect(0, 0, screen.width, screen.height);
+			
+			self.endAngles.forEach((item, index) => {
+				self._draw_faded_ring(index);
+				self._draw_ring(index);
+				
+				var direction = self._getDirection(index);
+				self._updateAngle(index, direction)
+			});
+			
+			if (!self._isComplete()) {
+				requestAnimationFrame(animate);
+			}
+			else {
+				if (self.onComplete) {
+					self.onComplete();
+				}
+			}
+		}
+		
+		animate();
+	},
+	
+	_updateAngle: function(index, direction){
+		var end_degrees = this.endAngles[index].target;
+				
+		if (this.endAngles[index].current <= end_degrees) {
+			this.endAngles[index].current += direction * this.speed;
+		}
+		else {
+			this.endAngles[index]['complete'] = true;
+		}
+	},
+	
+	_draw_faded_ring: function(index){
+		var start_radians = 0 * (Math.PI / 180);
+		var end_radians = 360 * (Math.PI / 180);
+		
+		this.gl.beginPath();
+		this.gl.strokeStyle  = this.endAngles[index].faded_color;
+		this.gl.arc(this.x, this.y, this.radius + index * 10, start_radians, end_radians, false);
+		this.gl.stroke();
+	},
+	
+	_draw_ring: function(index) {
+		var start_radians = this.startAngle * (Math.PI / 180);
+		var end_radians = this.endAngles[index].current * (Math.PI / 180);
+		
+		this.gl.beginPath();
+		this.gl.strokeStyle  = this.endAngles[index].color;
+		this.gl.arc(this.x, this.y, this.radius + index * 10, start_radians, end_radians, false);
+		this.gl.stroke();
+	},
+		
+	_isComplete: function() {
+		results = this.endAngles.filter((x)=> x.complete == false);
+		return results.length == 0;
+	}
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+function initWebGL(canvas) {
+  var gl = null;
+  
+  // Try to grab the standard context. If it fails, fallback to experimental.
+  gl = canvas.getContext('2d')
+  
+  // If we don't have a GL context, give up now
+  if (!gl) {
+    alert('Unable to initialize WebGL. Your browser may not support it.');
+  }
+  
+  canvas.width = 200;
+  canvas.height = 200;
+  
+  return gl;
+}
+
+var gl = null;
+
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+
+function start() {
+  var canvas = document.getElementById('glCanvas');
+  var canvas2 = document.getElementById('glCanvas2');
+
+  // Initialize the GL context
+  gl = initWebGL(canvas);
+  gl2 = initWebGL(canvas2);
+    
+  // Only continue if WebGL is available and working
+  if (!gl || !gl2) {
+    return;
+  }
+
+  // Set clear color to black, fully opaque     
+  draw_objects();
+}
+
+function draw_objects() {
+	
+	var graph = new Graph({
+		x: 0,
+		y: 0,
+		radius: 20,
+		startAngle: 270,
+		endAngles: [
+		{
+			angle: 180, 
+			color: "#FFFFFF",
+			faded_color: "#AAAAAA"
+		},
+		{
+			angle: 75,
+			color: "#DDFFDD",
+			faded_color: "#AABBAA"
+		},
+		{
+			angle: 90,
+		},
+		{
+			angle: 360,
+			color: "#660066",
+			faded_color: "#665566",
+		},
+		80
+		],
+	}, gl);
+	graph.draw();
+	
+	var graph2 = new Graph({
+		x: 0,
+		y: 0,
+		startAngle: 270,
+		radius: 20,
+		endAngles: [40, 70, 360, 90, 80],
+		speed: 3
+	}, gl2);
+	
+	graph2.draw();
+	
+	
+}
+window.onload = start;
